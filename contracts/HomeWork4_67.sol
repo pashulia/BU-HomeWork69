ab2cb35b7d68b020b296c353a474c7fc759da188
Pavel Khegay <pashulia.pk@gmail.com>
2023-05-25 11:54:56 +0600

    start project

---
+++ contracts/HomeWork4_67.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity 0.8.19;
+
+contract HomeWork4 {
+
+    struct Payment{
+        uint256 value;
+        address target;
+    }
+
+    uint256 public persent;
+    address public owner;
+
+    mapping(address => Payment) payments;
+
+    event AddPayment(uint256 value, address sender, address target);
+    event GetPayment(uint256 value, address sender, address target);
+
+    constructor(Payment memory payment, uint256 _persent){
+        owner = msg.sender;
+        payments[msg.sender] = payment;
+        persent = _persent;
+    }
+
+    function addPayment(address target) public payable {
+        // console.log("Contract: addPayment, msg.sender: ", msg.sender);
+        require(payments[msg.sender].value == 0, "You've already made a payment");
+        uint256 commission = msg.value * persent / 100;
+        payments[msg.sender] = Payment(msg.value - commission, target);
+        payments[owner].value += commission;
+        emit AddPayment(msg.value - commission, msg.sender, target);
+    }
+
+    function sendPayment(address sender) public returns(bool) {
+        // console.log("Contract: sendPayment, msg.sender: ", msg.sender);
+        Payment memory payment = payments[sender];
+        require(payment.target == msg.sender, "There are no payments for you");
+        bool successful = payable(payment.target).send(payment.value);
+        delete payments[sender];
+        emit GetPayment(payment.value, sender, payment.target);
+        return successful;
+    }
+
+    function getPayment(address sender) public view returns(Payment memory){
+        return payments[sender];
+    }
+}
---
+++ hardhat.config.js
@@ -0,0 +1,37 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("./tasks/task.js");
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.19",
+  defaultNetwork: "goerli",
+  networks:{
+    hardhat:{
+      loggingEnabled: false,
+      forking: {
+        url: process.env.GOERLI_RPC_URL,
+        // по умолчанию форк идёт с последнего блока, но можно указать явно 
+        // blockNumber: 1000
+      }
+    }, 
+    goerli:{
+      url: process.env.GOERLI_RPC_URL,
+      accounts: [ process.env.PIVATE_KEY ]
+    }
+  },
+  // gasReporter: {
+  //   enabled: true,
+  //   coinmarketcap: process.env.COINMARKEYCAP_KEY,
+  //   gasPriceApi: process.env.GOERLI_RPC_URL,
+  //   currency: "KZT",
+  //   showTimeSpent: true
+  // },
+  etherscan: {
+    apiKey: {
+        goerli: process.env.ETHERSCAN_API_KEY
+    }
+  }
+};
